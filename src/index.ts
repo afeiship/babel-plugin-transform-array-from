4f0efbe0537000f770dc207086a487f65b330a84
afeiship <1290657123@qq.com>
2024-04-20 21:07:52 +0800

    feat: add comments for src

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,14 @@
 const isOverflowed = (el: HTMLElement): boolean => {
   if (!el || el.clientWidth === 0) return false;
   const { clientWidth } = el;
+  // 1. create a clone of the element
   const cloneEl = el.cloneNode(true) as HTMLElement;
   cloneEl.style.position = 'absolute';
   cloneEl.style.top = '-9999px';
-  // insert after the original element
+  // 2. insert after the original element
   el.parentElement!.insertBefore(cloneEl, el.nextSibling);
   const { scrollWidth } = cloneEl;
-  // remove the clone element
+  // 3. remove the clone element
   el.parentElement!.removeChild(cloneEl);
   return scrollWidth > clientWidth;
 };
